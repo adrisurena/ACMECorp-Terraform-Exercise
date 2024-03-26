e8ae6226e349c7541b7e5f8ef775a0711790c08b
Adris <adris20021@gmail.com>
2024-03-26 18:08:32 -0400

    added in example for DevOps team

---
+++ Modules/DevOps/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.42.0"
+      
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "nginx-devops-acme-terraform" {
+    ami = "ami-0c101f26f147fa7fd"
+    instance_type = "t2.micro"
+  
+  tags = {
+    Name = "TerraformTechnicalExercise"
+}
+}
