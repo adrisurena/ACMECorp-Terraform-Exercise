3ff62a4583047f3a97889c01613fcf85e468a35b
Adris <adris20021@gmail.com>
2024-03-26 21:13:00 -0400

    Boilerplate for Network ACL

---
+++ Modules/Security/main.tf
@@ -0,0 +1,43 @@
+// Setting up the templating for adding in VPC's
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.42.0"
+    }
+  }
+}
+
+provider "aws" {
+region = "us-east-1"
+}
+
+    resource "aws_vpc" "mainvpc" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = aws_vpc.mainvpc.default_network_acl_id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
+
+// Set to continue once move to Terraform Cloud is done 
+
